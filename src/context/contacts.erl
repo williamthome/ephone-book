1f1135e732adfe0138f21cfc47cd7e30142f9d69
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-22 14:35:14 -0300

    feat: create contacts context

---
+++ src/context/contacts.erl
@@ -0,0 +1,13 @@
+-module(contacts).
+-export([create_contacts_table/0]).
+
+create_contacts_table() ->
+  {ok, Db} = ephone_book_db:connect(),
+  Query = "
+    CREATE TABLE IF NOT EXISTS contacts(
+      id bigserial PRIMARY KEY,
+      name VARCHAR(50) NOT NULL
+    );
+  ",
+  {ok, [], []} = ephone_book_db:query(Db, Query),
+  ok = ephone_book_db:disconnect(Db).
