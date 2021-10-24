f02fe80b937847097eebc2010ffb72e84ad0f3e4
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-24 15:16:52 -0300

    feat: render a hardcoded contact list

--- src/contacts_list_handler.erl
+++ src/contacts_list_handler.erl
@@ -4,6 +4,22 @@
 
 -define(HTML_PATH, "index.html").
 
+-record(contact, {
+  name,
+  phone
+}).
+
 init(Req, Opts) ->
-  Contacts = "My contacts",
-  ephone_book_server:html_response(Req, Opts, ?HTML_PATH, [Contacts]).
+  Contacts = [
+    #contact{name = "Foo", phone = "123"},
+    #contact{name = "Bar", phone = "456"},
+    #contact{name = "Foobar", phone = "789"}
+  ],
+  ContactsAsHtml = lists:map(fun contact_as_html/1, Contacts),
+  ephone_book_server:html_response(Req, Opts, ?HTML_PATH, [ContactsAsHtml]).
+
+contact_as_html(#contact{name = Name, phone = Phone}) ->
+  io_lib:format(
+    "<li class=\"contact\"><strong>~s</strong> - ~s</li>",
+    [Name, Phone]
+  ).
