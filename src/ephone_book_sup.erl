5c17cc6477bb2a4787f848c9afe4bf24aceb9d22
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-25 12:52:58 -0300

    refactor: include db gen_server in application supervisor children

--- src/ephone_book_sup.erl
+++ src/ephone_book_sup.erl
@@ -14,7 +14,7 @@
 -define(SERVER, ?MODULE).
 
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %% sup_flags() = #{strategy => strategy(),         % optional
 %%                 intensity => non_neg_integer(), % optional
@@ -26,10 +26,15 @@ start_link() ->
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
+  SupFlags = #{strategy => one_for_all,
+               intensity => 0,
+               period => 1},
+  ChildSpecs = [
+    #{
+      id => ephone_book_db,
+      start => {ephone_book_db, start_link, []}
+    }
+  ],
+  {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
