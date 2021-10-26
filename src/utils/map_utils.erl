a4456b4c1a563e79a0e1acab7f46af66e923c723
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-26 11:47:11 -0300

    refactor: add private fields option to the cast function from map_utils

--- src/utils/map_utils.erl
+++ src/utils/map_utils.erl
@@ -1,20 +1,37 @@
 -module(map_utils).
 
--export([cast/2]).
+-export([test/0]).
+-export([cast/2, cast/3]).
 
-cast(Map, Fields)
-  when is_map(Map) ->
+test() ->
+  Map = #{foo => "foo", bar => "bar", invalid_key => ignore, private => ignore},
+  Fields = [foo, bar],
+  PrivateFields = [private],
+  #{foo := "foo", bar := "bar"} = cast(Map, Fields, PrivateFields),
+  ok.
+
+cast(Map, Fields) ->
+  cast(Map, Fields, []).
+
+cast(Map, Fields, PrivateFields)
+  when is_map(Map)
+  andalso is_list(PrivateFields) ->
     KeyValueList = maps:to_list(Map),
-    cast(Map, Fields, KeyValueList, #{}).
+    cast(Map, Fields, PrivateFields, KeyValueList, #{}).
 
-cast(Map, Fields, [{Key, Value} | KeyValueList], ValidData) ->
-  case lists:member(Key, Fields) of
+cast(Map, Fields, PrivateFields, [{Key, Value} | KeyValueList], ValidData) ->
+  case valid_field(Key, Fields, PrivateFields) of
     true ->
       NewValidData = maps:put(Key, Value, ValidData),
-      cast(Map, Fields, KeyValueList, NewValidData);
+      cast(Map, Fields, PrivateFields, KeyValueList, NewValidData);
     false ->
-      cast(Map, Fields, KeyValueList, ValidData)
+      cast(Map, Fields, PrivateFields, KeyValueList, ValidData)
   end;
 
-cast(_Map, _Fields, [], ValidData) ->
+cast(_Map, _Fields, _PrivateFields, [], ValidData) ->
   ValidData.
+
+valid_field(Key, Fields, PrivateFields) ->
+  IsKey = lists:member(Key, Fields),
+  IsPrivate = lists:member(Key, PrivateFields),
+  IsKey and not IsPrivate.
