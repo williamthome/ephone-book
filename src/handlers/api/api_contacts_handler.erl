159d37755157069792959ca4421c51617abddc8d
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-26 14:51:26 -0300

    refactor: insert to contact_storage from api_contacts_handler route

--- src/handlers/api/api_contacts_handler.erl
+++ src/handlers/api/api_contacts_handler.erl
@@ -5,9 +5,19 @@
 -define(POST_METHOD, <<"POST">>).
 
 init(#{method := ?POST_METHOD} = Req, Opts) ->
-  {ok, Contact} = ephone_book_server:read_and_match_urlencoded_body(
-    [{name, nonempty}, {number, nonempty}],
+  Body = ephone_book_server:read_and_match_urlencoded_body(
+    [{name, nonempty}, {phone, nonempty}],
     Req
   ),
-  io:format("Contact to insert: ~p~n", [Contact]),
+  case Body of
+    {ok, Payload} ->
+      case contacts_storage:new(Payload) of
+        {ok, Contact} ->
+          io:format("Contact created: ~p~n", [Contact]);
+        Error ->
+          io:format("Error: ~p~n", [Error])
+      end;
+    Error ->
+      io:format("Error: ~p~n", [Error])
+  end,
   {ok, Req, Opts}.
