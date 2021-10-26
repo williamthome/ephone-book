86f28af826d22f86ca2f98a9b8530e4c11975f41
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-26 14:15:26 -0300

    feat: add a map_to_record function to map_utils

--- src/utils/map_utils.erl
+++ src/utils/map_utils.erl
@@ -1,13 +1,20 @@
 -module(map_utils).
 
--export([test/0]).
--export([cast/2, cast/3]).
+-export([test/0, test_map_to_record/0]).
+-export([cast/2, cast/3, map_to_record/3]).
 
 test() ->
   Map = #{foo => "foo", bar => "bar", invalid_key => ignore, private => ignore},
   Fields = [foo, bar],
   PrivateFields = [private],
   #{foo := "foo", bar := "bar"} = cast(Map, Fields, PrivateFields),
+  ok.
+
+test_map_to_record() ->
+  Map = #{foo => "foo", bar => "bar"},
+  Record = map_to_record(Map, foobar, [foo, bar]),
+  io:format("Record: ~p~n", [Record]),
+  {foobar, "foo", "bar"} = Record,
   ok.
 
 cast(Map, Fields) ->
@@ -35,3 +42,10 @@ valid_field(Key, Fields, PrivateFields) ->
   IsKey = lists:member(Key, Fields),
   IsPrivate = lists:member(Key, PrivateFields),
   IsKey and not IsPrivate.
+
+map_to_record(Map, RecordType, RecordFields)
+  when is_map(Map)
+  andalso is_atom(RecordType)
+  andalso is_list(RecordFields) ->
+    RecordValues = [maps:get(Key, Map, undefined) || Key <- RecordFields],
+    list_to_tuple([RecordType | RecordValues]).
