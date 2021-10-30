bd537c31140d6c1cd0aed5e1a075836a35a77d8d
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-30 02:24:29 -0300

    chore: live reload option to watch assets in priv folder

--- ephone_book.sh
+++ ephone_book.sh
@@ -6,15 +6,25 @@ case "$1" in
   run)
     case "$2" in
       dev)
-        echo "\n---------------------------------------------------------------"
-        echo "Live reload will be enabled."
-        echo "Important:"
-        echo "- xdotool is required $ sudo apt-get install xdotool"
-        echo "- The command refreshes the active tab in the browser"
-        echo "- Browser name is hardcoded at dev.escript as \"Navigator\""
-                # see https://stackoverflow.com/a/47704119/14166584
-        echo "p.s: Tested and working well using Firefox"
-        echo "---------------------------------------------------------------\n"
+        echo "
+        ---------------------------------------------------------------
+
+        Live reload will be enabled.
+
+        Tip:
+        Split the terminal and run the watch command to
+        reload assets in priv folder.
+
+        Important:
+        - xdotool is required $ sudo apt-get install xdotool
+        - The command refreshes the active tab in the browser
+        - Browser name is hardcoded at dev.escript as \"Navigator\".
+          See https://stackoverflow.com/a/47704119/14166584
+
+        p.s: Tested and working well using Firefox
+
+        ---------------------------------------------------------------
+        "
 
         rebar3 shell --script dev.escript
         ;;
@@ -25,8 +35,38 @@ case "$1" in
         ;;
     esac
     ;;
+  watch)
+    # Requires inotify-tools
+    # See https://unix.stackexchange.com/questions/323901/how-to-use-inotifywait-to-watch-a-directory-for-creation-of-files-of-a-specific/606213#606213
+
+    # Could use multiple folders
+    # e.g. WatchDir="src priv"
+    WatchDir=priv
+    BuildDir=./_build/default/lib/ephone_book/
+
+    echo "Watching $WatchDir for file changes..."
+
+    inotifywait \
+      $WatchDir \
+      -qrm \
+      -e close_write \
+      --format './%w%f %w' \
+      --include "(.*?)\.(html|css|js)$" \
+    | while read file dir; do
+      outdir="$BuildDir$dir"
+      echo "Copying $file to $outdir"
+
+      set +e
+      cp $file $outdir
+      set -e
+
+      export CurrentWindowId=$(xdotool getactivewindow)
+      xdotool search --onlyvisible --classname Navigator windowactivate --sync key F5
+      xdotool windowactivate $CurrentWindowId
+    done
+    ;;
   *)
-    echo "Please chose a command in [run]"
+    echo "Please chose a command in [run, watch]"
 
     exit 1
     ;;
