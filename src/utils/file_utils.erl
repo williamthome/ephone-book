8a1e554d8cf5da4bdbb058b8468ee6e185fc5eb0
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-31 15:53:47 -0300

    refactor: rename and export priv_dir_concat/1

--- src/utils/file_utils.erl
+++ src/utils/file_utils.erl
@@ -1,21 +1,21 @@
 -module(file_utils).
 
--export([read_file_from_priv_dir/1, read_file_from_priv_dir/2]).
+-export([priv_dir_concat/1, read_file_from_priv_dir/1, read_file_from_priv_dir/2]).
 
 -define(PROJECT_NAME, ephone_book).
 -define(PRIV_DIR, code:priv_dir(?PROJECT_NAME)).
 
-priv_dir_file(Path) ->
+priv_dir_concat(Path) ->
   filename:join([?PRIV_DIR, Path]).
 
 read_file_from_priv_dir(Path) ->
-  AbsPath = priv_dir_file(Path),
+  AbsPath = priv_dir_concat(Path),
   File = file:read_file(AbsPath),
   file_binary_or_not_found_func(File, AbsPath).
 
 read_file_from_priv_dir(Path, FuncIfNotFound)
   when is_function(FuncIfNotFound, 1) ->
-    AbsPath = priv_dir_file(Path),
+    AbsPath = priv_dir_concat(Path),
     File = file:read_file(AbsPath),
     file_binary_or_not_found_func(File, AbsPath, FuncIfNotFound).
 
