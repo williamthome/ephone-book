b70475ac65d016319e5ff2ac53565a7ed9cb774b
William Fank Thomé <{ID}+{username}@users.noreply.github.com>
2021-10-31 16:11:20 -0300

    fix: stop the server when app stops

--- src/ephone_book_app.erl
+++ src/ephone_book_app.erl
@@ -14,7 +14,7 @@ start(_StartType, _StartArgs) ->
   ephone_book_sup:start_link().
 
 stop(_State) ->
-  % ok = ephone_book_server:stop().
+  ok = ephone_book_server:stop(),
   ok.
 
 %% internal functions
